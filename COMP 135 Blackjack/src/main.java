c4c96ec041c0718f5512d7b744b07131040a6e1c
Maximo <m_macchi@u.pacific.edu>
2017-05-21 22:48:07 -0700

    Created Blackjack class. Implemented deal card functions for player and computer

--- COMP 135 Blackjack/src/main.java
+++ COMP 135 Blackjack/src/main.java
@@ -1,38 +1,3 @@
-import java.util.Random;
-
 public class main {
-	
-	private static final int JACK = 10;
-	private static final int QUEEN = 10;
-	private static final int KING = 10;
-	
-	private int playerTotal = 0;
-	private int compTotal = 0;
-	
-	public void dealFirstCards() {
-		for (int i = 0; i < 2; i++) {
-			int num = generateRandNum();
-			switch (num) {
-			case 11:
-				playerTotal += JACK;
-				break;
-			case 12:
-				playerTotal += QUEEN;
-				break;
-			case 13:
-				playerTotal += KING;
-				break;
-			default:
-				playerTotal += num;
-				break;
-			}
-		}
-	}
-	
-	public int generateRandNum() {
-		Random rand = new Random();
-		int num = rand.nextInt(13) + 1;
-		return num;
-	}
 	
 }
