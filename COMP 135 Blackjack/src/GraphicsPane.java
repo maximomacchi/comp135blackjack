57b1de5ee7cbc6ba3ace4a5f7846aa288da6d512
Maximo <m_macchi@u.pacific.edu>
2017-05-24 00:12:51 -0700

    Added GraphicsPane.java

---
+++ COMP 135 Blackjack/src/GraphicsPane.java
@@ -0,0 +1,68 @@
+
+
+//GraphicsPane.java
+//Provided by Osvaldo Jimenez
+//Added on 11/3/17
+//Maintained by Jack Thias
+
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public abstract class GraphicsPane implements Interfaceable {
+	@Override
+	public abstract void showContents();
+
+	@Override
+	public abstract void hideContents();
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	public void run() {
+		
+	}
+
+}
