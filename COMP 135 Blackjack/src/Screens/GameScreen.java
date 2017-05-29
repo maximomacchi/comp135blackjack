8c21dff1e725e8eb3d6a000871a06df455f1e65d
Maximo <m_macchi@u.pacific.edu>
2017-05-29 16:58:01 -0700

    Created updatLabel() function

--- COMP 135 Blackjack/src/Screens/GameScreen.java
+++ COMP 135 Blackjack/src/Screens/GameScreen.java
@@ -6,17 +6,21 @@ import Blackjack.GButton;
 import Blackjack.GraphicsPane;
 import Blackjack.main;
 import acm.graphics.GImage;
+import acm.graphics.GLabel;
 import acm.graphics.GObject;
 
 public class GameScreen extends GraphicsPane {
 	private main program;
 	private GImage background;
+	private GLabel playerScore;
+	private GLabel compScore;
 	private GButton hitButton;
 	
 	public GameScreen(main app) {
 		program = app;
 		background = new GImage("files/Card Dealt.png", 0, 0);
 		hitButton = new GButton(32, 489, 106, 72, false);
+		playerScore = new GLabel(Integer.toString(0), 469, 417);
 	}
 	
 	@Override
@@ -40,5 +44,12 @@ public class GameScreen extends GraphicsPane {
 				
 			}
 		}
+	}
+	
+	public void updateLabel(int newValue, GLabel label) {
+		program.remove(label);
+		GLabel newLabel = new GLabel(Integer.toString(newValue), label.getX(), label.getY());
+		program.add(newLabel);
+		return;
 	}
 }
