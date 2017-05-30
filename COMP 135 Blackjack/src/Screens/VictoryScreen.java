cf16a1b0ceab3500770db1af42d249baa6b0eb56
Maximo <m_macchi@u.pacific.edu>
2017-05-29 17:29:38 -0700

    Victory screen implemented

--- COMP 135 Blackjack/src/Screens/VictoryScreen.java
+++ COMP 135 Blackjack/src/Screens/VictoryScreen.java
@@ -1,19 +1,26 @@
 package Screens;
 
 import Blackjack.GraphicsPane;
+import Blackjack.main;
+import acm.graphics.GImage;
 
 public class VictoryScreen extends GraphicsPane {
-
+	private main program;
+	private GImage background;
 	
+	public VictoryScreen(main app) {
+		program = app;
+		background = new GImage("files/Victory.png", 0, 0);
+	}
 	
 	@Override
 	public void showContents() {
-		
+		program.add(background);
 	}
 
 	@Override
 	public void hideContents() {
-		
+		program.remove(background);
 	}
 	
 }
