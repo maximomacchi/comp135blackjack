84ab009d906a88e705f0601514a1f861faea138e
Maximo <m_macchi@u.pacific.edu>
2017-05-31 16:19:18 -0700

    Exit button on defeat screen implemented

--- COMP 135 Blackjack/src/Screens/DefeatScreen.java
+++ COMP 135 Blackjack/src/Screens/DefeatScreen.java
@@ -1,21 +1,31 @@
 package Screens;
 
 import java.awt.Color;
+import java.awt.event.MouseEvent;
 
+import Blackjack.GButton;
 import Blackjack.GraphicsPane;
 import Blackjack.main;
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
+import acm.graphics.GObject;
 
 public class DefeatScreen extends GraphicsPane {
 	private main program;
 	private GImage background;
 	private GLabel playerScore;
 	private GLabel compScore;
+	private GButton exitButton;
+	
+	private static final double EXIT_X = 524;
+	private static final double EXIT_Y = 405;
+	private static final double EXIT_HEIGHT = 102;
+	private static final double EXIT_WIDTH = 186;
 	
 	public DefeatScreen(main app) {
 		program = app;
 		background = new GImage("files/Defeat.png", 0, 0);
+		exitButton = new GButton(EXIT_X, EXIT_Y, EXIT_WIDTH, EXIT_HEIGHT, false);
 	}
 	
 	@Override
@@ -31,6 +41,7 @@ public class DefeatScreen extends GraphicsPane {
 		program.add(background);
 		program.add(playerScore);
 		program.add(compScore);
+		program.add(exitButton);
 	}
 
 	@Override
@@ -38,6 +49,15 @@ public class DefeatScreen extends GraphicsPane {
 		program.remove(background);
 		program.remove(playerScore);
 		program.remove(compScore);
+		program.remove(exitButton);
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		if (obj == exitButton) {
+			System.exit(1);
+		}
 	}
 	
 	public int getPlayerTotal() {
