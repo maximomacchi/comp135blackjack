62159e0d5279c71cdbcd9cc49babdb8c0ba7ecb6
Maximo <m_macchi@u.pacific.edu>
2017-06-01 15:08:30 -0700

    Added instructions button to main menu screen

--- COMP 135 Blackjack/src/Screens/MenuScreen.java
+++ COMP 135 Blackjack/src/Screens/MenuScreen.java
@@ -15,32 +15,36 @@ public class MenuScreen extends GraphicsPane {
 	private main program;
 	private GImage background;
 	private GButton instructionsButton;
+	private GButton playButton;
 	
-	private static final double INSTRUCTIONS_WIDTH_FACTOR = 2.354;
-	private static final double INSTRUCTIONS_HEIGHT_FACTOR = 1.816;
-	private static final double INSTRUCTIONS_SIZE_WIDTH = 117;
-	private static final double INSTRUCTIONS_SIZE_HEIGHT = 82;
-	private static final double INSTRUCTIONS_X = 435;
-	private static final double INSTRUCTIONS_Y = 423;
-	private final double INSTRUCTIONS_WIDTH_OFFSET = program.WINDOW_WIDTH/INSTRUCTIONS_WIDTH_FACTOR;
-	private final double INSTRUCTIONS_HEIGHT_OFFSET = program.WINDOW_HEIGHT/INSTRUCTIONS_HEIGHT_FACTOR;
+	private static final double INSTRUCTIONS_SIZE_WIDTH = 225;
+	private static final double INSTRUCTIONS_SIZE_HEIGHT = 108;
+	private static final double INSTRUCTIONS_X = 776;
+	private static final double INSTRUCTIONS_Y = 27;
+	private static final double PLAY_SIZE_WIDTH = 114;
+	private static final double PLAY_SIZE_HEIGHT = 77;
+	private static final double PLAY_X = 452;
+	private static final double PLAY_Y = 446;
 	
 	public MenuScreen(main app) {
 		program = app;
 		background = new GImage("files/Start Screen.png", 0, 0);
 		instructionsButton = new GButton(INSTRUCTIONS_X, INSTRUCTIONS_Y, INSTRUCTIONS_SIZE_WIDTH, INSTRUCTIONS_SIZE_HEIGHT, false);
+		playButton = new GButton(PLAY_X, PLAY_Y, PLAY_SIZE_WIDTH, PLAY_SIZE_HEIGHT, false);
 	}
 	
 	@Override
 	public void showContents() {
 		program.add(background);
 		program.add(instructionsButton);
+		program.add(playButton);
 	}
 	
 	@Override
 	public void hideContents() {
 		program.remove(background);
 		program.remove(instructionsButton);
+		program.remove(playButton);
 	}
 	
 	@Override
@@ -48,6 +52,9 @@ public class MenuScreen extends GraphicsPane {
 		GObject obj = program.getElementAt(e.getX(), e.getY());
 		if(obj == instructionsButton) {
 			program.switchToInstructions();
+		}
+		if (obj == playButton) {
+			program.switchToGame();
 		}
 	}
 
